ea58568002ee917f0c2dcdcb4052a726ce70e586
Abraham Ciokler <aciokler007@yahoo.com>
2025-01-18 12:18:21 -0500

    fix: Remove Findglm.cmake

--- cmake/Findglm.cmake
+++
@@ -1,3 +0,0 @@
-add_library(glm STATIC IMPORTED)
-find_library(GLM_LIBRARY_PATH glm HINTS "${CMAKE_CURRENT_LIST_DIR}/../include/")
-set_target_properties(glm PROPERTIES IMPORTED_LOCATION "${GLM_LIBRARY_PATH}")
